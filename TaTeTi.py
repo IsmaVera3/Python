4feace8dbb68cc96600c6da6567e449383687ddb
IsmaVera3 <132963276+IsmaVera3@users.noreply.github.com>
2024-03-07 14:41:59 -0300

    Update TaTeTi.py

--- TaTeTi.py
+++ TaTeTi.py
@@ -1,4 +1,5 @@
-from random import*
+import random
+import sys
 
 
 
@@ -18,26 +19,33 @@ while True:
     try:
         p1 = input("Ingrese su nombre jugador 1 : ")
         int(p1)
-        print("Por favor, ingrese un nombre válido, no un número.")
+        print("Recuerda que no puedes introducir numeros")
     except ValueError:
         break
 while True:
     try:
         p2 = input("Ingrese su nombre jugador 2: ")
         int(p2)
-        print("Por favor, ingrese un nombre válido, no un número.")
+        print("Recuerda que no puedes introducir numeros")
     except ValueError:
         break
 
+jugRan = [p1,p2]
+
 print(f'Bienvenidos {p1} y {p2}')
 
-jugadores = [p1,p2,p1,p2,p1,p2,p1,p2,p1,p2]
-juga = ['x','o','x','o','x','o','x','o','x','o']
 
+jugadores = [p1,p2,p1,p2,p1,p2,p1,p2,p1,p2]
+jugadores[0] = random.choice(jugRan)
+
+juga = ['x','o','x','o','x','o','x','o','x','o']
+print(tablero[0])
+print(tablero[1])
+print(tablero[2])
 def juego(jugadores,x,y,tablero,juga):
     while True:
         for i in range(9):
-            print(f'Turno del jugador: {jugadores[i]} vas a ser {juga[i]}')
+            print(f'Turno del jugador: {jugadores[i]} tu ficha es:  {juga[i]}')
             x = int(input('En que rango de x quieres jugar? = '))
             y = int(input('En que rango de y quieres jugar? = '))
             if (x <= 3 and x >= 1) and (y <= 3 and y >= 1):
@@ -54,57 +62,31 @@ def juego(jugadores,x,y,tablero,juga):
             print(tablero[0])
             print(tablero[1])
             print(tablero[2])
-            if tablero[0][0] == 'x' and tablero[0][1] == 'x' and tablero[0][2] == 'x':
-                print(f'Felicitaciones {jugadores[0]} has ganado')
-                break
-            if tablero[0][0] == 'x' and tablero[1][0] == 'x' and tablero[2][0] == 'x':
-                print(f'Felicitaciones {jugadores[0]} has ganado')
-                break
-            if tablero[0][2] == 'x' and tablero[1][2] == 'x' and tablero[2][2] == 'x':
-                print(f'Felicitaciones {jugadores[0]} has ganado')
-                break
-            if tablero[2][0] == 'x' and tablero[2][1] == 'x' and tablero[2][2] == 'x':
-                print(f'Felicitaciones {jugadores[0]} has ganado')
-                break
-            if tablero[0][0] == 'x' and tablero[1][1] == 'x' and tablero[2][2] == 'x':
-                print(f'Felicitaciones {jugadores[0]} has ganado')
-                break
-            if tablero[0][2] == 'x' and tablero[1][1] == 'x' and tablero[2][0] == 'x':
-                print(f'Felicitaciones {jugadores[0]} has ganado')
-                break
-            if tablero[1][0] == 'x' and tablero[1][1] == 'x' and tablero[1][2] == 'x':
-                print(f'Felicitaciones {jugadores[0]} has ganado')
-                break
-            if tablero[0][1] == 'x' and tablero[1][1] == 'x' and tablero[2][1] == 'x':
-                print(f'Felicitaciones {jugadores[0]} has ganado')
-                break
-
-
-
-            if tablero[0][0] == 'o' and tablero[0][1] == 'o' and tablero[0][2] == 'o':
-                print(f'Felicitaciones {jugadores[1]} has ganado')
-                break
-            if tablero[0][0] == 'o' and tablero[1][0] == 'o' and tablero[2][0] == 'o':
-                print(f'Felicitaciones {jugadores[1]} has ganado')
-                break
-            if tablero[0][2] == 'o' and tablero[1][2] == 'o' and tablero[2][2] == 'o':
-                print(f'Felicitaciones {jugadores[1]} has ganado')
-                break
-            if tablero[2][0] == 'o' and tablero[2][1] == 'o' and tablero[2][2] == 'o':
-                print(f'Felicitaciones {jugadores[1]} has ganado')
-                break
-            if tablero[0][0] == 'o' and tablero[1][1] == 'o' and tablero[2][2] == 'o':
-                print(f'Felicitaciones {jugadores[1]} has ganado')
-                break
-            if tablero[0][2] == 'o' and tablero[1][1] == 'o' and tablero[2][0] == 'o':
-                print(f'Felicitaciones {jugadores[1]} has ganado')
-                break
-            if tablero[1][0] == 'o' and tablero[1][1] == 'o' and tablero[1][2] == 'o':
-                print(f'Felicitaciones {jugadores[1]} has ganado')
-                break
-            if tablero[0][1] == 'o' and tablero[1][1] == 'o' and tablero[2][1] == 'o':
-                print(f'Felicitaciones {jugadores[1]} has ganado')
-                break
+            if (tablero[0][0] == 'x' and tablero[0][1] == 'x' and tablero[0][2] == 'x') or (tablero[0][0] == 'o' and tablero[0][1] == 'o' and tablero[0][2] == 'o'):
+                print(f'Felicitaciones {jugadores[i]} has ganado')
+                sys.exit()
+            if (tablero[0][0] == 'x' and tablero[1][0] == 'x' and tablero[2][0] == 'x') or (tablero[0][0] == 'o' and tablero[1][0] == 'o' and tablero[2][0] == 'o'):
+                print(f'Felicitaciones {jugadores[i]} has ganado')
+                sys.exit()
+            if (tablero[0][2] == 'x' and tablero[1][2] == 'x' and tablero[2][2] == 'x') or (tablero[0][2] == 'o' and tablero[1][2] == 'o' and tablero[2][2] == 'o'):
+                print(f'Felicitaciones {jugadores[i]} has ganado')
+                sys.exit()
+            if (tablero[2][0] == 'x' and tablero[2][1] == 'x' and tablero[2][2] == 'x') or (tablero[2][0] == 'o' and tablero[2][1] == 'o' and tablero[2][2] == 'o'):
+                print(f'Felicitaciones {jugadores[i]} has ganado')
+                sys.exit()
+            if (tablero[0][0] == 'x' and tablero[1][1] == 'x' and tablero[2][2] == 'x') or (tablero[0][0] == 'o' and tablero[1][1] == 'o' and tablero[2][2] == 'o'):
+                print(f'Felicitaciones {jugadores[i]} has ganado')
+                sys.exit()
+            if (tablero[0][2] == 'x' and tablero[1][1] == 'x' and tablero[2][0] == 'x') or (tablero[0][2] == 'o' and tablero[1][1] == 'o' and tablero[2][0] == 'o'):
+                print(f'Felicitaciones {jugadores[i]} has ganado')
+                sys.exit()
+            if (tablero[1][0] == 'x' and tablero[1][1] == 'x' and tablero[1][2] == 'x') or (tablero[1][0] == 'o' and tablero[1][1] == 'o' and tablero[1][2] == 'o'):
+                print(f'Felicitaciones {jugadores[i]} has ganado')
+                sys.exit()
+            if (tablero[0][1] == 'x' and tablero[1][1] == 'x' and tablero[2][1] == 'x') or (tablero[0][1] == 'o' and tablero[1][1] == 'o' and tablero[2][1] == 'o'):
+                print(f'Felicitaciones {jugadores[i]} has ganado')
+                sys.exit()
+        print(f'Es un Empate!')
         break
     return False
 
